5062fad1e22aab04106b7ffcd42484566532e20e
nicosanchez29 <nicolas.sanchezc@uqvirtual.edu.co>
2026-05-23 16:43:27 -0500

    Se agrega el proyecto final POO

---
+++ src/main/java/co/edu/uniquindio/poo/model/InmoSmart.java
@@ -0,0 +1,20 @@
+package co.edu.uniquindio.poo.model;
+
+import java.util.ArrayList;
+
+public class InmoSmart {
+
+    //ATRIBUTOS
+
+    private String nombre;
+    private String codigo;
+
+    //RELACIONES
+
+    private ArrayList <Usuario> listaUsuarios;
+    private ArrayList <Transaccion> listaTransaccion;
+    private ArrayList <Inmueble> listaInmuebles;
+    private ArrayList <Publicacion> listaPublicaciones;
+    private ArrayList <Oferta> listaOfertas;
+
+}
