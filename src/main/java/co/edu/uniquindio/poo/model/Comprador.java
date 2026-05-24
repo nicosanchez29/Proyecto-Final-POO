c1c00123da5d101693cd793ced909331e44f7702
kevinRuiz69 <kevina.ruizc@uqvirtual.edu.co>
2026-05-24 01:40:57 -0500

    CLASES Y ENUMS

--- src/main/java/co/edu/uniquindio/poo/model/Comprador.java
+++ src/main/java/co/edu/uniquindio/poo/model/Comprador.java
@@ -1,4 +1,13 @@
 package co.edu.uniquindio.poo.model;
 
-public class Comprador {
+public class Comprador extends Usuario {
+    public Comprador(String id, String nombre, String identificacion, String telefono, String correo, int puntosReputacion, CategoriaUsuario categoriaUsuario){
+super(id, nombre, identificacion, telefono, correo, puntosReputacion, categoriaUsuario);
+    }
+
+    @Override
+    public double calcularBeneficio() {
+        return 0;
+    }
+
 }
