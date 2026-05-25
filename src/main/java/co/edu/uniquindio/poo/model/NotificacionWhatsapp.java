338ffd152dbcfd306e91002672f67a455b667313
kevinRuiz69 <kevina.ruizc@uqvirtual.edu.co>
2026-05-25 00:07:37 -0500

    Notificacion

--- src/main/java/co/edu/uniquindio/poo/model/NotificacionWhatsapp.java
+++ src/main/java/co/edu/uniquindio/poo/model/NotificacionWhatsapp.java
@@ -1,4 +1,11 @@
 package co.edu.uniquindio.poo.model;
 
-public class NotificacionWhatsapp {
+public class NotificacionWhatsapp extends Notificacion{
+    public NotificacionWhatsapp(String mensaje, CanalNotificacion canalNotificacion, Usuario usuario) {
+        super(mensaje, canalNotificacion, usuario);
+    }
+    @Override
+    public void enviarNotificacion() {
+
+    }
 }
